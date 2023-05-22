b016232b748c0fd0fa79f36ab8af09d3df6ecd6f
Dante <ddperezp03@gmail.com>
2023-05-22 09:19:41 -0600

    sign in with google

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                RentXpert
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    Contrato Rentas
+                </span>
+            </h1>
+            <p className="desc text-center">
+                Tu plataforma en línea para crear contratos de renta de manera
+                fácil y segura.
+            </p>
+        </section>
+    );
+};
+
+export default Home;
